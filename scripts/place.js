cfcc914b80d960a5fd2f0703624dc6d8a26399a9
Mileski-Lucas <lucas.m_47@hotmail.com>
2025-03-28 17:30:12 -0300

    review in place

--- scripts/place.js
+++ scripts/place.js
@@ -1,21 +1,23 @@
-//calculate wind chill
+// Calculate wind chill
 function calculateWindChill(temp, windSpeed) {
     return (13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16)).toFixed(1);
 }
 
-//Displays wind chill
+// Display wind chill (using fixed values)
 function displayWindChill() {
-    const temperature = 17.6; // Celsius
+    const temperature = 6; // Celsius
     const windSpeed = 8; // km/h
     const windChillElement = document.getElementById('wind-chill');
 
-    if (temperature <= 10 && windSpeed > 4.8) {
-        windChillElement.textContent = calculateWindChill(temperature, windSpeed) + 'ºC';
-    } else {
-        windChillElement.textContent = "N/A";
+    if (windChillElement) {
+        if (temperature <= 10 && windSpeed > 4.8) {
+            windChillElement.textContent = calculateWindChill(temperature, windSpeed) + 'ºC';
+        } 
+        else {
+            windChillElement.textContent = 'N/A';
+        }
     }
 }
-
 
 function displayFooterDate() {
     const year = new Date().getFullYear();
@@ -25,6 +27,7 @@ function displayFooterDate() {
     document.getElementById('last-modified').textContent = lastModified;
 }
 
+// Run when the page loads
 document.addEventListener('DOMContentLoaded', () => {
     displayWindChill();
     displayFooterDate();
