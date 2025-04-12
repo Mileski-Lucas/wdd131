076b59793fcef99e777de9ea1b4cb65d33ea5441
Mileski-Lucas <lucas.m_47@hotmail.com>
2025-04-12 16:24:05 -0300

    git hub page test 1

---
+++ project/scripts/navigation.js
@@ -0,0 +1,14 @@
+document.getElementById('menu-button').addEventListener('click', function() {
+    const navMenu = document.getElementById('nav-menu');
+    const menuButton = this;
+
+    navMenu.classList.toggle('active');
+    menuButton.classList.toggle('open');
+
+    if (menuButton.classList.contains('open')) {
+        menuButton.textContent = '✕'; // Close icon
+    } else {
+        menuButton.textContent = '☰'; // Hamburger icon
+    }
+
+});
